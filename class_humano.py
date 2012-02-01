b563aad1ecae02227b3be7a983f57a7631acd91f
Danilo Chilene <bicofino@gmail.com>
2012-02-01 18:21:32 -0200

    alteracoes

--- class_humano.py
+++ class_humano.py
@@ -1,24 +1,30 @@
 # coding: utf-8
+'''
+Uma classe simples demonstrando atributos
+'''
 
 class Humano(object):
-    bipede    = True
-    carnivoro = False
-    profissao = None
+    fulano    = 'Fulano'
+
     
     def __init__(self, nome, sexo='M'):
+        assert sexo in ('M', 'F'), u'SEXO INVÁLIDO %s escolha M ou F' % sexo
         self.nome = nome
         self.sexo = sexo
-        assert sexo in ('M', 'F'), u'SEXO INVÁLIDO %s escolha M ou F' % sexo
-        
+        self.bipede    = True
+        self.carnivoro = False
+        self.profissao = None
+       
+
     def sexo(self):
-        return "Macho" if self.sexo=='M' else u'Femêa' 
-        
+        return "Macho" if self.sexo == 'M' else u'Femêa' 
+    
     def dieta(self, carnivoro):
         if carnivoro == True:
             print self.nome, u'É carnivoro'
         else:
             print self.nome, u'É vegetariano'
-            
+
     def profissao(self, profissao):
         if self.profissao is None:
             return u'Não informado'
@@ -28,10 +34,7 @@ class Humano(object):
         self.profissao = profissao
         
     def __str__(self):
-        return u'Nome:\t%s \nSexo:\t%s \nProfissao:\t%s\n' % (self.nome, self.sexo, self.profissao)
-        
-    def __repr__(self):
-        return u'Humano({0!r})'.format(self.nome, self.sexo, self.profissao)
+        return 'Nome:\t%s \nSexo:\t%s \nProfissao:\t%s\n' % (self.nome, self.sexo, self.profissao)
 
 if __name__ == "__main__":
     Humano1 = Humano('Juca')
