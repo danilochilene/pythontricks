97edec7ff1f49377b1457f88a1938b88b3a1f944
Danilo Chilene <bicofino@gmail.com>
2012-02-02 11:00:56 -0200

    Alteracoes class_humano.py adicionado contador e correcao relativo ao sexo

--- class_humano.py
+++ class_humano.py
@@ -1,46 +1,52 @@
+#! /usr/bin/env python
 # coding: utf-8
 '''
 Uma classe simples demonstrando atributos
 '''
+CONTADOR = 0
 
 class Humano(object):
-    fulano    = 'Fulano'
-
     
     def __init__(self, nome, sexo='M'):
-        assert sexo in ('M', 'F'), u'SEXO INVÁLIDO %s escolha M ou F' % sexo
+        global CONTADOR
+        assert sexo in ('M', 'F'), 'SEXO INVALIDO %s escolha M ou F' % sexo
+        CONTADOR += 1
+        self.id = CONTADOR
         self.nome = nome
-        self.sexo = sexo
+        self._sexo = sexo
         self.bipede    = True
         self.carnivoro = False
         self.profissao = None
-       
 
+    @property   
     def sexo(self):
-        return "Macho" if self.sexo == 'M' else u'Femêa' 
+        return "Macho" if self._sexo == 'M' else 'Femea' 
     
+    @property
     def dieta(self, carnivoro):
         if carnivoro == True:
-            print self.nome, u'É carnivoro'
+            print self.nome, 'Eh carnivoro'
         else:
-            print self.nome, u'É vegetariano'
+            print self.nome, 'Eh vegetariano'
 
     def profissao(self, profissao):
         if self.profissao is None:
-            return u'Não informado'
+            return 'Nao informado'
         return self.profissao
     
     def carreira(self, profissao):
         self.profissao = profissao
         
     def __str__(self):
-        return 'Nome:\t%s \nSexo:\t%s \nProfissao:\t%s\n' % (self.nome, self.sexo, self.profissao)
+        return 'ID:\t\t%s \nNome:\t\t%s \nSexo:\t\t%s \nProfissao:\t%s\n' % (self.id, self.nome, self.sexo, self.profissao)
 
 if __name__ == "__main__":
     Humano1 = Humano('Juca')
     Humano1.carreira('Peao')
-    Humano2 = Humano('Pedro')
-    Humano2.carreira('Medico')
+    Humano2 = Humano('Maria', 'F')
+    Humano2.carreira('Dona de casa')
+    Humano3 = Humano('Lucas')
+    Humano3.carreira('Advogado')
     
-    for object_ in (Humano1, Humano2):
+    for object_ in (Humano1, Humano2, Humano3):
         print object_
